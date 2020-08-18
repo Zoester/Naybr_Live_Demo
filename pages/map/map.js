b0933f65cf52b8d8a498d2ae7a51bf168008b564
Youyi Wang <youyi0206@126.com>
2020-08-18 21:37:36 +0800

    added map function on a separate page

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,20 +1,64 @@
 // pages/map/map.js
+const app = getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    events:[],
+    markers: [{
+    }],
+    userLongitude:'121.446648',
+    userLatitude:'31.218967'
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    let Cards = new wx.BaaS.TableObject('productCard')
+    // 1- 100 is offset 0
+    Cards.limit(100).offset(0).find().then((res) => {
+      console.log('card content', res.data.objects);
+      let markers = res.data.objects.map((event, index) => {
+          return {
+            latitude:event.latitude,
+            longitude:event.longitude,
+            id: event.id,
+            height: 50,
+            width: 50,
+            iconPath: "../images/Home.png"
+          }
+      }).filter((event)=>{
+        return event.latitude && event.longitude
+      })
+      console.log('marker',markers)
+      this.setData({
+        events: res.data.objects,
+        markers
+      })
+    })
 
+    this.getLocation();
+  
   },
 
+  getLocation: function() {
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        const speed = res.speed
+        const accuracy = res.accuracy
+        this.setData({
+          userLongitude: longitude,
+          userLatitude: latitude
+        })
+      }
+     })
+  },
   /**
    * Lifecycle function--Called when page is initially rendered
    */
